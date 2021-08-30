0cdb50bbe898c42249951814ccba1c1102741071
ashhmen <ashhmen@gmail.com>
2021-08-30 06:05:38 +0200

    hah

--- src/main/java/com/mycompany/report/service/ReportService.java
+++ src/main/java/com/mycompany/report/service/ReportService.java
@@ -3,16 +3,18 @@ package com.mycompany.report.service;
 
 import com.mycompany.myapp.domain.Author;
 import com.mycompany.myapp.repository.AuthorRepository;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
+//import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import java.util.Map;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -23,11 +25,25 @@ public class ReportService {
 
 
     public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        String path= "C:\\Users\\PC-A\\Documents";
         List<Author> author=repository.findAll();
         //load file and compile
         File file= ResourceUtils.getFile("classpath:author.jrxml");
         JasperDesign jasperDesign;
         JasperReport jasperReport=JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource=new JRBeanCollectionDataSource(author);
+        Map<String,Object> parameters= new HashMap<>();
+        parameters.put("createdBy","Ashref Elarayed");
+        JasperPrint jasperPrint= JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if(reportFormat.equalsIgnoreCase("html")){
+            JasperExportManager.exportReportToHtmlFile(jasperPrint,path+"\\author.html");
+        }
+        if(reportFormat.equalsIgnoreCase("pdf")){
+            JasperExportManager.exportReportToHtmlFile(jasperPrint,path+"\\author.pdf");
+        }
+        return "report generated in path: "+ path;
 
     }
+
+
 }
